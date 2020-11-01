9d25fbc9361c4acf8a9c0ffa083c84a49b93fc11
Bilalov Timur <t.bilalov@corp.mail.ru>
2020-11-01 19:58:38 +0300

    grouping legend items, part 3

--- src/components/data/Fetch.svelte
+++ src/components/data/Fetch.svelte
@@ -23,6 +23,7 @@
     let calcMethodSaved = calcMethod;
     let usdData = Storage.get(STORAGE_KEYS.usdData) || [];
     let legendItems = [];
+    let datasetsColors = {};
 
     $: {
         // TODO: Хз как добиться внятного поведения без такого хака. Поизучать...
@@ -312,8 +313,16 @@
     }
 
     function prepareSingleDataset(title, data, amount, isUsd) {
+        const sameLabelDatasets = datasets.filter(item => item.label.toLowerCase() === title.toLowerCase());
         const getRandomNumber = () => Math.round(Math.random() * 255);
-        let colorRGB = [getRandomNumber(), getRandomNumber(), getRandomNumber()];
+
+        let colorRGB;
+        if (sameLabelDatasets.length > 1 && datasetsColors[title] !== undefined) {
+            colorRGB = datasetsColors[title];
+        } else {
+            colorRGB = [getRandomNumber(), getRandomNumber(), getRandomNumber()];
+            datasetsColors[title] = colorRGB;
+        }
         let opacity = 0.6;
         let borderWidth = 1;
 
@@ -515,14 +524,45 @@
                     intersect: false,
                     mode: 'index',
                     callbacks: {
-                        label: function(tooltipItem, myData) {
-                            var label = myData.datasets[tooltipItem.datasetIndex].label || '';
-                            if (label) {
-                                label += ': ';
-                            }
-                            label += parseFloat(tooltipItem.value).toFixed(2);
+                        label: function(tooltipItem, data) {
+                            const dataset = data.datasets[tooltipItem.datasetIndex];
+                            const label = dataset.label;
+                            const date = tooltipItem.label;
+                            const sameLabelDatasets = data.datasets.filter(item => item.label === label);
 
-                            return label;
+                            const nonZeroLabelValues = (function () {
+                                const index = sameLabelDatasets[0].dates.indexOf(date);
+                                return sameLabelDatasets.filter(item => !!item.data[index]).map(item => item.data[index]);
+                            })();
+                            let labelText = '';
+
+                            if (label === undefined) {
+                                return '';
+                            }
+
+                            const value = tooltipItem.value;
+
+                            sameLabelDatasets[0]._tooltipSameIndexes = sameLabelDatasets[0]._tooltipSameIndexes || [];
+                            if (!sameLabelDatasets[0]._tooltipSameIndexes.includes(tooltipItem.datasetIndex)) {
+                                sameLabelDatasets[0]._tooltipSameIndexes.push(tooltipItem.datasetIndex);
+                            }
+
+                            labelText = `${label}: ${parseFloat(value).toFixed(2)}`;
+                            if (nonZeroLabelValues.length > 1) {
+                                if (sameLabelDatasets[0]._tooltipSameIndexes.indexOf(tooltipItem.datasetIndex) > 0) {
+                                    labelText = undefined;
+                                } else {
+                                    labelText = `${label}: [${nonZeroLabelValues.map(item => item.toFixed(2)).join(',')}]`;
+
+                                    if (calcMethod === CALC_METHODS.RELATIVE) {
+                                        labelText += `, avg ${(nonZeroLabelValues.reduce((p, c) => p + c) / nonZeroLabelValues.length).toFixed(2)}`;
+                                    } else if (calcMethod === CALC_METHODS.ABSOLUTE || calcMethod === CALC_METHODS.ABSOLUTE_TOTAL) {
+                                        labelText += `, total ${(nonZeroLabelValues.reduce((p, c) => p + c)).toFixed(2)}`;
+                                    }
+                                }
+                            }
+
+                            return labelText;
                         }
                     }
                 },
@@ -634,11 +674,11 @@
     .controls {
         position: fixed;
         left: 10px;
-        top: 10px;
+        top: 40px;
         background-color: #fff;
         padding: 10px;
         overflow: auto;
-        max-height: 100%;
+        max-height: calc(100% - 40px);
         box-sizing: border-box;
         z-index: 2;
         box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
